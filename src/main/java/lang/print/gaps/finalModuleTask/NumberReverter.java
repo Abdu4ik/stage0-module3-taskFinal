6b8efc9e0b4b92df7d61b21065f52eafb0383bdd
Abdu4ik <107094143+Abdu4ik@users.noreply.github.com>
2022-08-20 03:38:29 +0500

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int f = number / 100, s = (number / 10) % 10, l = number % 10;
+
+        System.out.println(l + "" + (s) + "" + f);
+
     }
 }
