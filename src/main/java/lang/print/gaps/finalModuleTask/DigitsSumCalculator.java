1ace1658b2fd6d38ce2ead618a360c23273379bd
Abdu4ik <107094143+Abdu4ik@users.noreply.github.com>
2022-08-20 03:37:40 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int f = number / 100, s = (number / 10) % 10, stl = (number / 100) % 10 ,l = number % 10;
+
+        System.out.println(f+s+stl+l);
     }
 }
